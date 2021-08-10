a28fdee21ae7a36650c39e2ae70d4f0ed57a2b79
Saro <saropk@yahoo.com>
2021-08-10 17:18:14 +0530

    First Update#1

--- FlaskApp.py
+++ FlaskApp.py
@@ -4,12 +4,14 @@ from bson import ObjectId  # For ObjectId to work
 from pymongo import MongoClient
 import os
 import logging
-import orderInsert
+import MangoDb_Sample
 
 app = Flask(__name__)
 
 @app.route('/')
 def getFunc():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
